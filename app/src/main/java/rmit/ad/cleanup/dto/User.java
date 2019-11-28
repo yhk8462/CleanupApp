89bf2d3730630e880a1a43fb58d310cb7f362e13
heshaniperera <hepe@mazarin.lk>
2019-11-28 05:46:56 +0530

    Added the User DTO class

---
+++ app/src/main/java/rmit/ad/cleanup/dto/User.java
@@ -0,0 +1,71 @@
+package rmit.ad.cleanup.dto;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+    private String id;
+    private String name;
+    private String dateOfBirth;
+    private String phoneNumber;
+    private String email;
+    private String password;
+
+    public User(){}
+
+    public User(String name, String dateOfBirth, String phoneNumber, String email, String password){
+        this.setName(name);
+        this.setDateOfBirth(dateOfBirth);
+        this.setPhoneNumber(phoneNumber);
+        this.setEmail(email);
+        this.setPassword(password);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
